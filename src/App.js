4427f05cf40dffce60193b337c5785ada2e89488
agarwalutkarsh <utkarshagarwal1299@gmail.com>
2021-05-30 19:11:56 +0530

    initialize

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import {useDataLayerValue} from './DataLayer';
+
+const spotify=new SpotifyWebApi();
+
+function App() {
+  const [token,setToken]=useState(null);
+  const [{ user },dispatch]=useDataLayerValue();
+
+  useEffect(()=>{
+    const hash=getTokenFromUrl();
+    window.location.hash="";
+    const _token=hash.access_token;
+
+    if(_token){
+      setToken(_token);
+      
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user)=>{
+        dispatchEvent({
+          type:'SET_USER',
+          user:user,
+        })
+      });
+    }
+    console.log("I have a token >>> ",token);
+  }, []);
+
+  console.log(">>>",user);
+
+  return (
+    <div className="app">
+      {
+
+        token?(
+          <Player />
+        ):(
+          <Login />
+        )
+
+      }
+    </div>
+  );
+}
+
+export default App;
